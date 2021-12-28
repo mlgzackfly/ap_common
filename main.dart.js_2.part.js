6d6f51d54c57868a94e2f6f1042e596f50a1382f
CI <flutter-ci@github.com>
2021-12-28 07:40:09 +0000

    Updated site

---
+++ main.dart.js_2.part.js
@@ -0,0 +1,24 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={
+f(d){return new A.aq6(d)},
+nf:function nf(){},
+aq6:function aq6(d){this.a=d},
+bo3(d,e,f){if(d!=="")return d
+return e}},B
+a.setFunctionNamesIfNecessary([A])
+A=a.updateHolder(c[5],A)
+B=c[0]
+A.nf.prototype={
+ayr(d,e,f,g,h,i){var y=A.bo3(f,d,h),x=this.gLb().h(0,y)
+if(x==null)return d
+else return B.aSO(x,g,null)},
+h(d,e){return this.gLb().h(0,e)},
+i(d){return this.gZq()}}
+var z=a.updateTypes([])
+A.aq6.prototype={
+$0(){return this.a},
+$S:11};(function inheritance(){var y=a.inherit
+y(A.nf,B.P)
+y(A.aq6,B.Dh)})()
+B.aNz(b.typeUniverse,JSON.parse("{}"))}
+$__dart_deferred_initializers__["05seZg9CwEoj8+Q/5Cle+MzV01s="] = $__dart_deferred_initializers__.current
